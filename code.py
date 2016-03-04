cae61b07337d0f681a81cbea5ad6235b8f9417f2
ahmad da'na <ahmadjd94@gmail.com>
2016-03-04 23:55:14 +0200

    adding code

---
+++ code.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.2.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import (QMainWindow, QTextEdit, 
+    QAction, QFileDialog, QApplication)
+from PyQt5.QtGui import QIcon
+import sqlite3
+
+class Ui_MainWindow(object):
+    public :
+        global con
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(646, 434)
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+        self.analysis = QtWidgets.QTabWidget(self.centralWidget)
+        self.analysis.setGeometry(QtCore.QRect(0, 30, 641, 351))
+        self.analysis.setMouseTracking(False)
+        self.analysis.setObjectName("analysis")
+        self.tab = QtWidgets.QWidget()
+        self.tab.setObjectName("tab")
+        self.radioButton = QtWidgets.QRadioButton(self.tab)
+        self.radioButton.setGeometry(QtCore.QRect(70, 100, 198, 19))
+        self.radioButton.setChecked(True)
+        self.radioButton.setObjectName("radioButton")
+        self.radioButton_2 = QtWidgets.QRadioButton(self.tab)
+        self.radioButton_2.setGeometry(QtCore.QRect(70, 180, 198, 18))
+        self.radioButton_2.setObjectName("radioButton_2")
+        self.progressBar = QtWidgets.QProgressBar(self.tab)
+        self.progressBar.setGeometry(QtCore.QRect(70, 280, 511, 23))
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setObjectName("progressBar")
+        self.lineEdit = QtWidgets.QLineEdit(self.tab)
+        self.lineEdit.setGeometry(QtCore.QRect(280, 90, 281, 25))
+        self.lineEdit.setObjectName("lineEdit")
+        self.pushButton = QtWidgets.QPushButton(self.tab)
+        self.pushButton.setGeometry(QtCore.QRect(480, 230, 97, 27))
+        self.pushButton.setObjectName("pushButton")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.tab)
+        self.lineEdit_2.setGeometry(QtCore.QRect(280, 170, 281, 25))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.label = QtWidgets.QLabel(self.tab)
+        self.label.setGeometry(QtCore.QRect(70, 260, 351, 17))
+        self.label.setObjectName("label")
+        self.pushButton_2 = QtWidgets.QPushButton(self.tab)
+        self.pushButton_2.setGeometry(QtCore.QRect(581, 90, 29, 27))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.tab)
+        self.pushButton_3.setGeometry(QtCore.QRect(581, 171, 29, 27))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.analysis.addTab(self.tab, "")
+        self.tab_2 = QtWidgets.QWidget()
+        self.tab_2.setObjectName("tab_2")
+        self.analysis.addTab(self.tab_2, "")
+        MainWindow.setCentralWidget(self.centralWidget)
+        self.menuBar = QtWidgets.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 646, 27))
+        self.menuBar.setObjectName("menuBar")
+        self.menuBRO_visualizer = QtWidgets.QMenu(self.menuBar)
+        self.menuBRO_visualizer.setObjectName("menuBRO_visualizer")
+        self.menuHelp = QtWidgets.QMenu(self.menuBar)
+        self.menuHelp.setObjectName("menuHelp")
+        MainWindow.setMenuBar(self.menuBar)
+        self.mainToolBar = QtWidgets.QToolBar(MainWindow)
+        self.mainToolBar.setObjectName("mainToolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.mainToolBar)
+        self.statusBar = QtWidgets.QStatusBar(MainWindow)
+        self.statusBar.setObjectName("statusBar")
+        MainWindow.setStatusBar(self.statusBar)
+        self.actionAbout = QtWidgets.QAction(MainWindow)
+        self.actionAbout.setObjectName("actionAbout")
+        self.menuHelp.addSeparator()
+        self.menuHelp.addAction(self.actionAbout)
+        self.menuBar.addAction(self.menuBRO_visualizer.menuAction())
+        self.menuBar.addAction(self.menuHelp.menuAction())
+
+        self.retranslateUi(MainWindow)
+        self.analysis.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.radioButton.setText(_translate("MainWindow", "load single file"))
+        self.radioButton_2.setText(_translate("MainWindow", "load directory of log files"))
+        self.pushButton.setText(_translate("MainWindow", "Load"))
+        self.label.setText(_translate("MainWindow", "unable to load file , please check your file directory"))
+        self.pushButton_2.setText(_translate("MainWindow", "..."))
+        self.pushButton_3.setText(_translate("MainWindow", "..."))
+        self.analysis.setTabText(self.analysis.indexOf(self.tab), _translate("MainWindow", "Load Files"))
+        self.analysis.setTabText(self.analysis.indexOf(self.tab_2), _translate("MainWindow", "Tab 2"))
+        self.menuBRO_visualizer.setTitle(_translate("MainWindow", "BRO visualizer"))
+        self.menuHelp.setTitle(_translate("MainWindow", "help"))
+        self.mainToolBar.setWindowTitle(_translate("MainWindow", "BRO Log file analyzer and visualizer"))
+        self.actionAbout.setText(_translate("MainWindow", "about"))
+        self.pushButton_2.clicked.connect(self.a)
+
+    def openFileDialog(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')
+
+        if fname[0]:
+            f = open(fname[0], 'r')
+
+            with f:
+                data = f.read()
+                self.textEdit.setText(data)  
+    def openDirDialog(self):
+
+        pass
+
+    def openFile(self,file):
+        if radioButton.isChecked
+            try:
+                f= open(lineEdit.value)
+                for i in f :
+                #should include REGEX to find the rquired patterns
+            except :
+                # launch a message box 
+        else :
+
+
+    def openDir():
+        os.chdir(lineEdit_2)
+        files=glob.glob()
+        for each in files :
+            openFile(each)
+
+
+
+if __name__ == "__main__":
+    import sys ,glob
+
+    try :
+        con=sqlite3.connect('analyze.db')
+    except :
+        # showmessgae box to tell the user to use the program with admin permissions 
+        sys.exit()
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
